984dda0e36974bb4585d3c801b987091ad380c58
Lyushen <78591028+Lyushen@users.noreply.github.com>
2023-10-28 20:11:13 +0100

    another fix of fix of fix

--- script/script.js
+++ script/script.js
@@ -7,26 +7,36 @@ document.addEventListener("DOMContentLoaded", async function () {
   imageWorker.onmessage = function(event) {
     const preloadedImage = URL.createObjectURL(event.data);
     imageQueue.push(preloadedImage);
-  };
-
-  pageWorker.onmessage = function(event) {
-    const pageContent = event.data;
-    const url = event.data.url;
-    pageCache[url] = pageContent;
-  };
-
-  imageWorker.postMessage('https://picsum.photos/1920/1080');
-  await loadPage('home.html');
-  
-  // Existing preloadFirstImage() and preloadFirstPage() functions are removed
-  
-  // Update background
-  setInterval(() => {
-    imageWorker.postMessage('https://picsum.photos/1920/1080');
     if (imageQueue.length > 1) {
       document.body.style.backgroundImage = `url(${imageQueue.shift()})`;
     }
+  };
+
+  pageWorker.onmessage = function(event) {
+    const url = event.data.url;
+    const pageContent = event.data.content;
+    pageCache[url] = pageContent;
+  };
+
+  // Kick-off background workers for initial tasks
+  imageWorker.postMessage('https://picsum.photos/1920/1080');
+  pageWorker.postMessage('home.html');
+
+  // Update background every 3000ms
+  setInterval(() => {
+    imageWorker.postMessage('https://picsum.photos/1920/1080');
   }, 3000);
+
+  // Function for loading pages
+  function loadPage(url) {
+    if (pageCache[url]) {
+      const content = document.getElementById('content');
+      content.innerHTML = pageCache[url];
+      content.classList.add('loaded');
+    } else {
+      pageWorker.postMessage(url);
+    }
+  }
 
   const navLinks = document.querySelectorAll('.topnav a');
   navLinks.forEach(link => {
@@ -41,50 +51,4 @@ document.addEventListener("DOMContentLoaded", async function () {
       }
     });
   });
-
-  
-  async function loadPage(url) {
-    if (pageCache[url]) {
-      const content = document.getElementById('content');
-      content.innerHTML = pageCache[url];
-      content.classList.add('loaded');
-    } else {
-      const xhr = new XMLHttpRequest();
-      xhr.onreadystatechange = function () {
-        if (xhr.readyState == 4 && xhr.status == 200) {
-          pageCache[url] = xhr.responseText;
-          const content = document.getElementById('content');
-          content.innerHTML = xhr.responseText;
-          content.classList.add('loaded');
-        }
-      };
-      xhr.open('GET', url, true);
-      xhr.send();
-    }
-  }
-
-  const navLinks = document.querySelectorAll('.topnav a');
-  navLinks.forEach(link => {
-    link.addEventListener('click', function (e) {
-      e.preventDefault();
-      loadPage(this.getAttribute('data-page'));
-    });
-    link.addEventListener('mouseover', function (e) {
-      const url = this.getAttribute('data-page');
-      if (!pageCache[url]) {
-        preloadPage(url);
-      }
-    });
-  });
-
-  function preloadPage(url) {
-    const xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function () {
-      if (xhr.readyState == 4 && xhr.status == 200) {
-        pageCache[url] = xhr.responseText;
-      }
-    };
-    xhr.open('GET', url, true);
-    xhr.send();
-  }
 });
