5627dc09d72589a0ca879eb1e1ff65aac51ff69c
Lyushen <78591028+Lyushen@users.noreply.github.com>
2023-10-28 12:53:17 +0100

    fixing what is not intended to work

--- script/script.js
+++ script/script.js
@@ -1,37 +1,15 @@
 document.addEventListener("DOMContentLoaded", function() {
-  // Function to preload image
-    function preloadImage(url, callback) {
-      const img = new Image();
-      img.src = url;
-      img.onload = function() {
-        callback(url);
-      };
-    }
-  // Function to load pages
-function loadPage(url) {
-  const content = document.getElementById('content');
-  
-  // Make content disappear smoothly
-  content.classList.remove('loaded');
-  content.classList.add('loading');
 
-  var xhr = new XMLHttpRequest();
-  xhr.onreadystatechange = function() {
-    if (xhr.readyState == 4 && xhr.status == 200) {
-      content.innerHTML = xhr.responseText;
+  // Function for preloading images
+  function preloadImage(url, callback) {
+    const img = new Image();
+    img.src = url;
+    img.onload = function() {
+      callback(url);
+    };
+  }
 
-      // Make content appear smoothly
-      setTimeout(() => {
-        content.classList.remove('loading');
-        content.classList.add('loaded');
-      }, 50);
-    }
-  };
-  xhr.open('GET', url, true);
-  xhr.send();
-}
-
-  // Function for background rotation
+  // Function for rotating background
   function updateBackground() {
     fetch('https://unsplash.it/1920/1080/?random')
       .then(response => {
@@ -42,25 +20,32 @@ function loadPage(url) {
       });
   }
 
-  // Update background initially and then every 3000ms
+  // Initialize and repeat background update
   updateBackground();
   setInterval(updateBackground, 3000);
 
+  // Function to load pages
   function loadPage(url) {
+    const content = document.getElementById('content');
+    content.style.opacity = 0;
+
     var xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function() {
       if (xhr.readyState == 4 && xhr.status == 200) {
-        const content = document.getElementById('content');
         content.innerHTML = xhr.responseText;
-        content.classList.add('loaded');
+        setTimeout(() => {
+          content.style.opacity = 1;
+        }, 50);
       }
     };
     xhr.open('GET', url, true);
     xhr.send();
   }
 
+  // Initial page load
   loadPage('home.html');
 
+  // Event listeners for navigation links
   document.querySelectorAll('.topnav a').forEach(link => {
     link.addEventListener('click', function(e) {
       e.preventDefault();
