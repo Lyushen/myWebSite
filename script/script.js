570d629e754b2b6fcc1004069ff479f89693dd2c
Lyushen <78591028+Lyushen@users.noreply.github.com>
2023-11-15 19:28:32 +0000

    some changes

--- script/script.js
+++ script/script.js
@@ -89,20 +89,3 @@ document.addEventListener("DOMContentLoaded", async function () {
   }
 
 });
-function openMenu(dropDownName) {
-  document.getElementById(String(dropDownName)).classList.toggle("show");
-}
-window.onclick = function(e) {
-  if (!e.target.matches('.dropbtn')) {
-  var myDropdown = document.getElementById("apprenticeshipID");
-    if (myDropdown.classList.contains('show')) {
-      myDropdown.classList.remove('show');
-    }
-  }
-  if (!e.target.matches('.dropbtn2')) {
-    var myDropdown2 = document.getElementById("balloonsID");
-      if (myDropdown2.classList.contains('show')) {
-        myDropdown2.classList.remove('show');
-      }
-    }
-}
