e70ba5945ded02080e7d2a8469a43feafa541fb6
Lyushen <78591028+Lyushen@users.noreply.github.com>
2023-11-15 09:25:55 +0000

    funny css

--- script/script.js
+++ script/script.js
@@ -100,9 +100,9 @@ window.onclick = function(e) {
     }
   }
   if (!e.target.matches('.dropbtn2')) {
-    var myDropdown = document.getElementById("myDropdown2");
-      if (myDropdown.classList.contains('show')) {
-        myDropdown.classList.remove('show');
+    var myDropdown2 = document.getElementById("myDropdown2");
+      if (myDropdown2.classList.contains('show')) {
+        myDropdown2.classList.remove('show');
       }
     }
 }
